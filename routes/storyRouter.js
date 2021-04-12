5d8a1312a4850f224fa30c04273939ebf910be94
Simanta Deb Turja <simantaturja@gmail.com>
2021-04-12 13:56:28 +0600

    clean code

--- routes/storyRouter.js
+++ routes/storyRouter.js
@@ -1,12 +1,10 @@
 const express = require('express');
 
-const mongoose = require('mongoose');
-
 const router = express.Router();
 
 const Story = require('../models/Story');
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
   const stories = await Story.find({});
   const refStories = stories.map((story) => {
     const refStory = story.toJSON();
@@ -18,34 +16,32 @@ router.get('/', async (req, res, next) => {
   res.send(refStories);
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', async (req, res) => {
   try {
     const story = await Story.findById(req.params.id);
     if (story) {
       res.statusCode = 200;
-      res.send(story);
-    } else {
-      res.statusCode = 404;
-      res.send('Story Not Found');
+      return res.send(story);
     }
+    res.statusCode = 404;
+    return res.send('Story Not Found');
   } catch (error) {
     res.statusCode = 404;
-    res.send('Story Not Found');
+    return res.send('Story Not Found');
   }
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', async (req, res) => {
   const createStory = await Story.create({
     title: req.body.title,
     body: req.body.body,
   });
   if (createStory) {
     res.statusCode = 201;
-    res.send(createStory);
-  } else {
-    res.statusCode = 500;
-    res.send('title and body must be included!!');
+    return res.send(createStory);
   }
+  res.statusCode = 500;
+  return res.send('title and body must be included!!');
 });
 
 module.exports = router;
